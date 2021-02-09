755d86de9ca5c2a5cbb1f3716430cc466c589867
Deydra Cavazos <deydra@cavazospartners.com>
2021-02-09 17:53:39 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,8 @@
+def reverse_each_word(sentence)
+  sentence_array = sentence.split(" ")
+  reverse_array = []
+  sentence_array.collect do |element|
+    reverse_array.push(element.reverse)
+  end
+  reverse_array.join(" ")
+end
